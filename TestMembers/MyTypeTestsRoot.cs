602dce640d32a2e09b4e928a2c5525603f3f0719
CsabaDu <csabadudas@t-email.hu>
2024-10-16 14:56:30 +0200

    MyTypeTestsRoot segregation with notations

--- TestMembers/MyTypeTestsRoot.cs
+++ TestMembers/MyTypeTestsRoot.cs
@@ -2,19 +2,30 @@ namespace CsabaDu.DynamicDataTests_NetConf2024.TestMembers;
 
 public abstract class MyTypeTestsRoot
 {
+    #region Test params values
+
     protected const string TestLabel = "TestLabel"; // UpperCase
     protected const string DifferentLabel = "testLabel"; // lowerCase
     protected const int TestQuantity = 3;
     protected const int DifferentQuantity = 4;
+    #endregion
+
+    #region SUT fields
 
     protected MyType _myType;
     protected int _quantity;
     protected string _label;
+    #endregion
+
+    #region Test params fields
 
     protected object _obj;
     protected MyType _other;
     protected int _hashCode1;
     protected int _hashCode2;
+    #endregion
+
+    #region Test helper methods
 
     protected void InitMyType()
     {
@@ -34,4 +45,5 @@ public abstract class MyTypeTestsRoot
         _hashCode1 = _myType.GetHashCode();
         _hashCode2 = other.GetHashCode();
     }
+    #endregion
 }
