29315e3d16c9e96296f70e5b1d828884f5f626bc
CsabaDu <csabadudas@t-email.hu>
2024-10-27 10:17:35 +0100

    DynamicDataSources correction

--- TestMembers/DynamicDataSources.cs
+++ TestMembers/DynamicDataSources.cs
@@ -15,13 +15,13 @@ public class DynamicDataSources : MyTypeTests
 
     private object[] ArgsToObjectArray_object(FrameworkCode frameworkCode)
     {
-        TestCase_bool_MyType args = new(_testCase, _expected, _other);
+        TestCase_bool_object args = new(_testCase, _expected, _obj);
         return args.ToObjectArray(frameworkCode);
     }
 
     private object[] ArgsToObjectArray_MyType(FrameworkCode frameworkCode)
     {
-        TestCase_bool_object args = new(_testCase, _expected, _other);
+        TestCase_bool_MyType args = new(_testCase, _expected, _other);
         return args.ToObjectArray(frameworkCode);
     }
     #endregion
@@ -33,22 +33,24 @@ public class DynamicDataSources : MyTypeTests
         _expected = true;
         _testCase = CreateTestCase("Same MyType");
         _obj = InitMyTypeElements();
-        yield return ArgsToObjectArray_object(frameworkCode);
+        yield return argsToObjectArray();
 
         _expected = false;
         _testCase = CreateTestCase("null");
         _obj = null;
-        yield return ArgsToObjectArray_object(frameworkCode);
+        yield return argsToObjectArray();
 
         _testCase = CreateTestCase("object");
         _obj = new();
-        yield return ArgsToObjectArray_object(frameworkCode);
+        yield return argsToObjectArray();
 
         _testCase = CreateTestCase("Different MyType");
         _quantity = DifferentQuantity;
         _label = DifferentLabel;
         _obj = GetMyType();
-        yield return ArgsToObjectArray_object(frameworkCode);
+        yield return argsToObjectArray();
+
+        object[] argsToObjectArray() => ArgsToObjectArray_object(frameworkCode);
     }
 
     public IEnumerable<object[]> GetGetHashCodeArgs(FrameworkCode frameworkCode)
@@ -56,19 +58,21 @@ public class DynamicDataSources : MyTypeTests
         _expected = true;
         _testCase = CreateTestCase("Same Quantity, same Label");
         _other = InitMyTypeElements();
-        yield return ArgsToObjectArray_MyType(frameworkCode);
+        yield return argsToObjectArray();
 
         _expected = false;
         _testCase = CreateTestCase("Different Quantity, same Label");
         _quantity = DifferentQuantity;
         _other = GetMyType();
-        yield return ArgsToObjectArray_MyType(frameworkCode);
+        yield return argsToObjectArray();
 
         _testCase = CreateTestCase("Same Quantity, different Label");
         _quantity = TestQuantity;
         _label = DifferentLabel;
         _other = GetMyType();
-        yield return ArgsToObjectArray_MyType(frameworkCode);
+        yield return argsToObjectArray();
+
+        object[] argsToObjectArray() => ArgsToObjectArray_MyType(frameworkCode);
     }
 
     public IEnumerable<object[]> GetEqualsMyTypeArgs(FrameworkCode frameworkCode)
