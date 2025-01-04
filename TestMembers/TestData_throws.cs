570ae6408d46d133db1467b6ed162403a753fc3c
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-04 06:45:57 +0100

    TestData_throws<TException>.Result corrected

--- TestMembers/TestData_throws.cs
+++ TestMembers/TestData_throws.cs
@@ -2,7 +2,7 @@
 {
     public abstract record TestData_throws<TException>(string ParamsDescription, TException Exception) : TestData<TException>(ParamsDescription) where TException : Exception
     {
-        protected override string Result => $"throws {typeof(Exception).Name}";
+        protected override string Result => $"throws {typeof(TException).Name}";
     }
 
     public abstract record TestData_throws(string ParamsDescription, Exception Exception) : TestData_throws<Exception>(ParamsDescription, Exception)
